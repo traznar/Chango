e18abd182bfc917cb099216e650637fbceaf6a8d
adrian <56691299+AdrianTEC@users.noreply.github.com>
2021-04-23 22:57:19 -0600

    Adding the build

---
+++ cmake-build-debug/CMakeFiles/DonkeyC.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DonkeyC.dir/graphics.c.obj"
+  "CMakeFiles/DonkeyC.dir/main.c.obj"
+  "DonkeyC.exe"
+  "DonkeyC.exe.manifest"
+  "DonkeyC.pdb"
+  "libDonkeyC.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/DonkeyC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
